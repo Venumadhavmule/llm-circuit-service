ec7d2d3eb35adfec2c106715b643683cdabeb8e3
Venumadhavmule <venumadhavamule@gmail.com>
2025-11-15 19:22:57 +0530

    Initial starter code

---
+++ src/main/java/com/venu/llm/circuit/LlmCircuitServiceApplication.java
@@ -0,0 +1,13 @@
+package com.venu.llm.circuit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmCircuitServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmCircuitServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venu/llm/circuit/LlmCircuitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venu.llm.circuit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmCircuitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
