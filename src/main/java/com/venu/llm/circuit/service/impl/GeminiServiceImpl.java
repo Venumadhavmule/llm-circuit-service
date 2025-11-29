a59277777776d321c00e78688846f0d50d113901
Venumadhavmule <venumadhavamule@gmail.com>
2025-11-29 23:56:53 +0530

    Reapply "New LLM MOdel:- Open Ai Llm Integration & Multi llm models support"
    This reverts commit aa5c856bb8ab93237eb43cbb2931cac1f590c53f.

--- src/main/java/com/venu/llm/circuit/service/impl/GeminiServiceImpl.java
+++ src/main/java/com/venu/llm/circuit/service/impl/GeminiServiceImpl.java
@@ -1,11 +1,9 @@
-package com.venu.llm.circuit.service.impl
-;
+package com.venu.llm.circuit.service.impl;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import com.google.genai.ResponseStream;
-import com.google.genai.types.GenerateContentResponse;
 import com.venu.llm.circuit.service.LlmService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -15,16 +13,16 @@ import reactor.core.publisher.Flux;
 @Slf4j
 public class GeminiServiceImpl implements LlmService {
 
-	private ChatClient chatClient;
+	private ChatClient geminiChatClient;
 
-	public GeminiServiceImpl(ChatClient chatClient) {
-		this.chatClient = chatClient;
+	public GeminiServiceImpl(@Qualifier("geminiChatClient") ChatClient chatClient) {
+		this.geminiChatClient = chatClient;
 	}
 
 	public String sendTextQuery(String prompt) {
 		log.info("Getting the request: {}", prompt);
 
-		String result = chatClient.prompt().user(prompt).call().content();
+		String result = geminiChatClient.prompt().user(prompt).call().content();
 
 		log.info("Returning the response: {}", result);
 		return result;
@@ -33,7 +31,7 @@ public class GeminiServiceImpl implements LlmService {
 	public Flux<String> streamTextQuery(String prompt) {
 		log.info("Starting streaming call for prompt: {}", prompt);
 
-		return chatClient.prompt().user(prompt).stream().content();
+		return geminiChatClient.prompt().user(prompt).stream().content();
 	}
 
 }
